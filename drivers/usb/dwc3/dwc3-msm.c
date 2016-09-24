f1ebd351bf1a9b8b8379d9ce32bcfbf19c578f39
Sultanxda <sultanxda@gmail.com>
2016-09-24 13:46:09 -0700

    usb: dwc3-msm: Remove indefinite wakeup
    Something is wrong with this wakeup; it isn't always relaxed, thus
    wreaking havoc on battery life. We already have a wakeup in smbcharger
    that is triggered on USB connection, so instead of fixing this one, just
    remove it since it's redundant.
    
    Change-Id: I9a902fbf0e9701abfc5a40f40f2a6e2cc6a4fd09
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2629,8 +2629,6 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc, bool force_power_collapse)
 		dev_dbg(mdwc->dev, "defer suspend with %d(msecs)\n",
 					mdwc->lpm_to_suspend_delay);
 		pm_wakeup_event(mdwc->dev, mdwc->lpm_to_suspend_delay);
-	} else {
-		pm_relax(mdwc->dev);
 	}
 
 	atomic_set(&dwc->in_lpm, 1);
@@ -2685,8 +2683,6 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		mutex_unlock(&mdwc->suspend_resume_mutex);
 		return 0;
 	}
-
-	pm_stay_awake(mdwc->dev);
 
 	if (mdwc->in_host_mode && mdwc->max_rh_port_speed == USB_SPEED_HIGH)
 		dwc3_msm_update_bus_bw(mdwc, BUS_VOTE_SVS);
