9028a6157228431775c263949d822f2b0fa5c4f2
John Dias <joaodias@google.com>
2016-10-20 10:28:32 -0700

    fs: Improve eventpoll logging to stop indicting timerfd
    timerfd doesn't create any wakelocks; eventpoll can, and is creating the
    wakelocks we see called "[timerfd]".  eventpoll creates two kinds of
    wakelocks: a single top-level lock associated with the eventpoll fd
    itself, and one additional lock for each fd it is polling that needs such
    a lock (e.g. those using EPOLLWAKEUP).  Current code names the per-fd
    locks using the undecorated names of the fds' associated files (hence
    "[timerfd]"), and is naming the top-level lock after the PID of the caller
    and the name of the file behind the first fd for which a per-fd lock is
    created.  To make things clearer, the top-level lock is now named using
    the caller PID and an "epollfd" designation, while the per-fd locks are
    also named with the caller's PID (to associate them with the top-level
    lock) and their respective fds' file names.
    
    Port of fix already applied to previous 2 generations.  Note that this
    set of changes does not fully solve the problem of eventpoll/timerfd
    wakelock attribution to the original process, since most activity is
    relayed through system_server, but it does at least ensure that different
    eventpoll wakelocks - and their stats - are properly disambiguated.
    
    Test: Ran on device and observed new wakelock naming in
    /d/wakeup_sources and (file naming in) lsof output.
    Bug: 116363986
    Change-Id: I34bada5ddab04cf3830762c745f46bfcd1549cb8
    Signed-off-by: John Dias <joaodias@google.com>
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1406,7 +1406,9 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	}
 
 	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
-	ws = wakeup_source_register(NULL, n.name);
+	snprintf(buf, sizeof(buf), "epoll_%.*s_file:%s",
+		 (int)sizeof(task_comm_buf), task_comm_buf, n.name);
+	ws = wakeup_source_register(NULL, buf);
 	release_dentry_name_snapshot(&n);
 
 	if (!ws)
