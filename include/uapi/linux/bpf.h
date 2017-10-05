47cee70157e61cbba3578b79f59a58f1fb359978
Yonghong Song <yhs@fb.com>
2017-10-05 09:19:20 -0700

    BACKPORT: bpf: add helper bpf_perf_event_read_value for perf event array map
    Hardware pmu counters are limited resources. When there are more
    pmu based perf events opened than available counters, kernel will
    multiplex these events so each event gets certain percentage
    (but not 100%) of the pmu time. In case that multiplexing happens,
    the number of samples or counter value will not reflect the
    case compared to no multiplexing. This makes comparison between
    different runs difficult.
    
    Typically, the number of samples or counter value should be
    normalized before comparing to other experiments. The typical
    normalization is done like:
      normalized_num_samples = num_samples * time_enabled / time_running
      normalized_counter_value = counter_value * time_enabled / time_running
    where time_enabled is the time enabled for event and time_running is
    the time running for event since last normalization.
    
    This patch adds helper bpf_perf_event_read_value for kprobed based perf
    event array map, to read perf counter and enabled/running time.
    The enabled/running time is accumulated since the perf event open.
    To achieve scaling factor between two bpf invocations, users
    can can use cpu_id as the key (which is typical for perf array usage model)
    to remember the previous value and do the calculation inside the
    bpf program.
    
    Change-Id: I74632f4b3b1ad086463883dab80f088ca1116edb
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Acked-by: Alexei Starovoitov <ast@fb.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -649,6 +649,14 @@ union bpf_attr {
  *     @delta: An positive/negative integer to be added to xdp_md.data_meta
  *     Return: 0 on success or negative on error
  *
+ * int bpf_perf_event_read_value(map, flags, buf, buf_size)
+ *     read perf event counter value and perf event enabled/running time
+ *     @map: pointer to perf_event_array map
+ *     @flags: index of event in the map or bitmask flags
+ *     @buf: buf to fill
+ *     @buf_size: size of the buf
+ *     Return: 0 on success or negative error code
+ *
  * int skb_load_bytes_relative(const struct sk_buff *skb, u32 offset, void *to, u32 len, u32 start_header)
  * 	Description
  * 		This helper is similar to **bpf_skb_load_bytes**\ () in that
@@ -843,7 +851,9 @@ enum bpf_func_id {
 #define BPF_F_ZERO_CSUM_TX		(1ULL << 1)
 #define BPF_F_DONT_FRAGMENT		(1ULL << 2)
 
-/* BPF_FUNC_perf_event_output and BPF_FUNC_perf_event_read flags. */
+/* BPF_FUNC_perf_event_output, BPF_FUNC_perf_event_read and
+ * BPF_FUNC_perf_event_read_value flags.
+ */
 #define BPF_F_INDEX_MASK		0xffffffffULL
 #define BPF_F_CURRENT_CPU		BPF_F_INDEX_MASK
 /* BPF_FUNC_perf_event_output for sk_buff input context. */
@@ -1040,5 +1050,11 @@ enum {
 
 #define TCP_BPF_IW		1001	/* Set TCP initial congestion window */
 #define TCP_BPF_SNDCWND_CLAMP	1002	/* Set sndcwnd_clamp */
+
+struct bpf_perf_event_value {
+	__u64 counter;
+	__u64 enabled;
+	__u64 running;
+};
 
 #endif /* _UAPI__LINUX_BPF_H__ */
