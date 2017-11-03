8b7d15ceb03455521fa3f0e1f801ea0e6437d0f4
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-11-03 13:56:18 -0700

    UPSTREAM: bpf: report offload info to user space
    Extend struct bpf_prog_info to contain information about program
    being bound to a device.  Since the netdev may get destroyed while
    program still exists we need a flag to indicate the program is
    loaded for a device, even if the device is gone.
    
    Change-Id: I3f62c7a66da2b1cd97fec2cd37e135c54d230337
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -541,6 +541,7 @@ static inline bool unprivileged_ebpf_enabled(void)
 
 int bpf_prog_offload_compile(struct bpf_prog *prog);
 void bpf_prog_offload_destroy(struct bpf_prog *prog);
+u32 bpf_prog_offload_ifindex(struct bpf_prog *prog);
 
 #if defined(CONFIG_NET) && defined(CONFIG_BPF_SYSCALL)
 int bpf_prog_offload_init(struct bpf_prog *prog, union bpf_attr *attr);
