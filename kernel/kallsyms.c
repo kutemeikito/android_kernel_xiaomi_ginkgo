54f4bd65414b28327e6726a46c9c8171327aa5d0
Linus Torvalds <torvalds@linux-foundation.org>
2017-11-08 12:51:04 -0800

    stop using '%pK' for /proc/kallsyms pointer values
    Not only is it annoying to have one single flag for all pointers, as if
    that was a global choice and all kernel pointers are the same, but %pK
    can't get the 'access' vs 'open' time check right anyway.
    
    So make the /proc/kallsyms pointer value code use logic specific to that
    particular file.  We do continue to honor kptr_restrict, but the default
    (which is unrestricted) is changed to instead take expected users into
    account, and restrict access by default.
    
    Right now the only actual expected user is kernel profiling, which has a
    separate sysctl flag for kernel profile access.  There may be others.
    
    Change-Id: Icebc329c4402ba9be03301b8b92110791708a246
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -658,6 +658,40 @@ static const struct seq_operations kallsyms_op = {
 	.show = s_show
 };
 
+static inline int kallsyms_for_perf(void)
+{
+#ifdef CONFIG_PERF_EVENTS
+	extern int sysctl_perf_event_paranoid;
+	if (sysctl_perf_event_paranoid <= 1)
+		return 1;
+#endif
+	return 0;
+}
+
+/*
+ * We show kallsyms information even to normal users if we've enabled
+ * kernel profiling and are explicitly not paranoid (so kptr_restrict
+ * is clear, and sysctl_perf_event_paranoid isn't set).
+ *
+ * Otherwise, require CAP_SYSLOG (assuming kptr_restrict isn't set to
+ * block even that).
+ */
+static int kallsyms_show_value(void)
+{
+	switch (kptr_restrict) {
+	case 0:
+		if (kallsyms_for_perf())
+			return 1;
+	/* fallthrough */
+	case 1:
+		if (has_capability_noaudit(current, CAP_SYSLOG))
+			return 1;
+	/* fallthrough */
+	default:
+		return 0;
+	}
+}
+
 static int kallsyms_open(struct inode *inode, struct file *file)
 {
 	/*
@@ -671,6 +705,7 @@ static int kallsyms_open(struct inode *inode, struct file *file)
 		return -ENOMEM;
 	reset_iter(iter, 0);
 
+	iter->show_value = kallsyms_show_value();
 	return 0;
 }
 
