be7e4900126f665cd82daaa9272ccb289682fb82
Linus Torvalds <torvalds@linux-foundation.org>
2017-11-12 18:44:23 -0800

    /proc/module: use the same logic as /proc/kallsyms for address exposure
    The (alleged) users of the module addresses are the same: kernel
    profiling.
    
    So just expose the same helper and format macros, and unify the logic.
    
    Change-Id: I517de86518e823f7ba17e916b3f3ab3e208ace94
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -526,6 +526,7 @@ struct kallsym_iter {
 	char name[KSYM_NAME_LEN];
 	char module_name[MODULE_NAME_LEN];
 	int exported;
+	int show_value;
 };
 
 static int get_ksymbol_mod(struct kallsym_iter *iter)
@@ -676,7 +677,7 @@ static inline int kallsyms_for_perf(void)
  * Otherwise, require CAP_SYSLOG (assuming kptr_restrict isn't set to
  * block even that).
  */
-static int kallsyms_show_value(void)
+int kallsyms_show_value(void)
 {
 	switch (kptr_restrict) {
 	case 0:
