b4ee887553335e2a94e4398f9c1c8d0c570a9c0d
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-11-20 15:21:56 -0800

    UPSTREAM: bpf: turn bpf_prog_get_type() into a wrapper
    bpf_prog_get_type() is identical to bpf_prog_get_type_dev(),
    with false passed as attach_drv.  Instead of keeping it as
    an exported symbol turn it into static inline wrapper.
    
    Change-Id: I0d5b738de9c3e533a2d869d04a98665162c3a602
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -350,7 +350,6 @@ extern const struct bpf_verifier_ops tc_cls_act_analyzer_ops;
 extern const struct bpf_verifier_ops xdp_analyzer_ops;
 
 struct bpf_prog *bpf_prog_get(u32 ufd);
-struct bpf_prog *bpf_prog_get_type(u32 ufd, enum bpf_prog_type type);
 struct bpf_prog *bpf_prog_get_type_dev(u32 ufd, enum bpf_prog_type type,
 				       bool attach_drv);
 struct bpf_prog * __must_check bpf_prog_add(struct bpf_prog *prog, int i);
@@ -443,12 +442,6 @@ static inline bool unprivileged_ebpf_enabled(void)
 
 #else /* !CONFIG_BPF_SYSCALL */
 static inline struct bpf_prog *bpf_prog_get(u32 ufd)
-{
-	return ERR_PTR(-EOPNOTSUPP);
-}
-
-static inline struct bpf_prog *bpf_prog_get_type(u32 ufd,
-						 enum bpf_prog_type type)
 {
 	return ERR_PTR(-EOPNOTSUPP);
 }
@@ -548,6 +541,12 @@ static inline bool unprivileged_ebpf_enabled(void)
 }
 
 #endif /* CONFIG_BPF_SYSCALL */
+
+static inline struct bpf_prog *bpf_prog_get_type(u32 ufd,
+						 enum bpf_prog_type type)
+{
+	return bpf_prog_get_type_dev(ufd, type, false);
+}
 
 int bpf_prog_offload_compile(struct bpf_prog *prog);
 void bpf_prog_offload_destroy(struct bpf_prog *prog);
