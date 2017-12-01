3bce0655b05d2dea8a23588e04d6a38eceb059aa
William Tu <u9012063@gmail.com>
2017-12-01 15:26:08 -0800

    BACKPORT: ip6_gre: add ip6 gre and gretap collect_md mode
    Similar to gre, vxlan, geneve, ipip tunnels, allow ip6 gre and gretap
    tunnels to operate in collect metadata mode.  bpf_skb_[gs]et_tunnel_key()
    helpers can make use of it right away.  OVS can use it as well in the
    future.
    
    Change-Id: I6c91a4734031d0fceddfe7ee2c80fea0abef87f6
    Signed-off-by: William Tu <u9012063@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- net/ipv6/ip6_tunnel.c
+++ net/ipv6/ip6_tunnel.c
@@ -879,7 +879,7 @@ int ip6_tnl_rcv(struct ip6_tnl *t, struct sk_buff *skb,
 	if (tpi->proto == htons(ETH_P_IP))
 		dscp_ecn_decapsulate = ip4ip6_dscp_ecn_decapsulate;
 
-	return __ip6_tnl_rcv(t, skb, tpi, NULL, dscp_ecn_decapsulate,
+	return __ip6_tnl_rcv(t, skb, tpi, tun_dst, dscp_ecn_decapsulate,
 			     log_ecn_err);
 }
 EXPORT_SYMBOL(ip6_tnl_rcv);
@@ -997,6 +997,9 @@ int ip6_tnl_xmit_ctl(struct ip6_tnl *t,
 	struct __ip6_tnl_parm *p = &t->parms;
 	int ret = 0;
 	struct net *net = t->net;
+
+	if (t->parms.collect_md)
+		return 1;
 
 	if ((p->flags & IP6_TNL_F_CAP_XMIT) ||
 	    ((p->flags & IP6_TNL_F_CAP_PER_PACKET) &&
