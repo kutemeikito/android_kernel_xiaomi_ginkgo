9940a89e0fdc12ab281b195e4932b5a25947a388
David Ahern <dsahern@gmail.com>
2018-04-20 15:37:58 -0700

    BACKPORT: net/ipv6: Rename rt6_get_cookie_safe
    rt6_get_cookie_safe takes a fib6_info and checks the sernum of
    the node. Update the name to reflect its purpose.
    
    Change-Id: I8326a60f9833841f2ea19efdd0c9b2297e0494bb
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/net/ip6_fib.h
+++ include/net/ip6_fib.h
@@ -228,8 +228,8 @@ static inline bool fib6_check_expired(const struct fib6_info *f6i)
  * Return true if we can get cookie safely
  * Return false if not
  */
-static inline bool rt6_get_cookie_safe(const struct fib6_info *f6i,
-				       u32 *cookie)
+static inline bool fib6_get_cookie_safe(const struct fib6_info *f6i,
+					u32 *cookie)
 {
 	struct fib6_node *fn;
 	bool status = false;
@@ -253,7 +253,7 @@ static inline u32 rt6_get_cookie(const struct rt6_info *rt)
 	u32 cookie = 0;
 
 	if (rt->from)
-		rt6_get_cookie_safe(rt->from, &cookie);
+		fib6_get_cookie_safe(rt->from, &cookie);
 
 	return cookie;
 }
