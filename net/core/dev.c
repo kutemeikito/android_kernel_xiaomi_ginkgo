bd8ae39b82765094dd941cee4a2b07480d8a1142
Nikita V. Shirokov <tehnerd@tehnerd.com>
2018-04-25 07:15:03 -0700

    UPSTREAM: bpf: fix xdp_generic for bpf_adjust_tail usecase
    When bpf_adjust_tail was introduced for generic xdp, it changed skb's tail
    pointer, so it was pointing to the new "end of the packet". However skb's
    len field wasn't properly modified, so on the wire ethernet frame had
    original (or even bigger, if adjust_head was used) size. This diff is
    fixing this.
    
    Fixes: 198d83bb3 (" bpf: make generic xdp compatible w/ bpf_xdp_adjust_tail")
    Change-Id: I8dc4a56d4e3206e76793ac757410c7619b0e2354
    Signed-off-by: Nikita V. Shirokov <tehnerd@tehnerd.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4033,8 +4033,10 @@ static u32 netif_receive_generic_xdp(struct sk_buff *skb,
 	 * pckt.
 	 */
 	off = orig_data_end - xdp.data_end;
-	if (off != 0)
+	if (off != 0) {
 		skb_set_tail_pointer(skb, xdp.data_end - xdp.data);
+		skb->len -= off;
+	}
 
 	switch (act) {
 	case XDP_REDIRECT:
