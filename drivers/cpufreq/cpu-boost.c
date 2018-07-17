94e9b457df4aa8a0580f1f3ada0b1579858053b4
Alex Naidis <alex.naidis@paranoidandroid.co>
2018-07-16 21:21:33 -0400

    cpu-boost: Reduce input boost time interval
    Reducing this interval allows us to not drop to lower frequencies during
    a regular scroll without finger release while using a relatively low boost
    ms value.
    
    Signed-off-by: Alex Naidis <alex.naidis@paranoidandroid.co>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -46,7 +46,8 @@ static bool sched_boost_active;
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
-#define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
+
+#define MIN_INPUT_INTERVAL (100 * USEC_PER_MSEC)
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
