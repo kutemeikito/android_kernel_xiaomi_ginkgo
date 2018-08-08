89ef752a4d02460533ebd5f091da8debbfc2c2c9
Martin KaFai Lau <kafai@fb.com>
2018-08-08 01:01:22 -0700

    BACKPORT: net: Add ID (if needed) to sock_reuseport and expose reuseport_lock
    A later patch will introduce a BPF_MAP_TYPE_REUSEPORT_ARRAY which
    allows a SO_REUSEPORT sk to be added to a bpf map.  When a sk
    is removed from reuse->socks[], it also needs to be removed from
    the bpf map.  Also, when adding a sk to a bpf map, the bpf
    map needs to ensure it is indeed in a reuse->socks[].
    Hence, reuseport_lock is needed by the bpf map to ensure its
    map_update_elem() and map_delete_elem() operations are in-sync with
    the reuse->socks[].  The BPF_MAP_TYPE_REUSEPORT_ARRAY map will only
    acquire the reuseport_lock after ensuring the adding sk is already
    in a reuseport group (i.e. reuse->socks[]).  The map_lookup_elem()
    will be lockless.
    
    This patch also adds an ID to sock_reuseport.  A later patch
    will introduce BPF_PROG_TYPE_SK_REUSEPORT which allows
    a bpf prog to select a sk from a bpf map.  It is inflexible to
    statically enforce a bpf map can only contain the sk belonging to
    a particular reuse->socks[] (i.e. same IP:PORT) during the bpf
    verification time. For example, think about the the map-in-map situation
    where the inner map can be dynamically changed in runtime and the outer
    map may have inner maps belonging to different reuseport groups.
    Hence, when the bpf prog (in the new BPF_PROG_TYPE_SK_REUSEPORT
    type) selects a sk,  this selected sk has to be checked to ensure it
    belongs to the requesting reuseport group (i.e. the group serving
    that IP:PORT).
    
    The "sk->sk_reuseport_cb" pointer cannot be used for this checking
    purpose because the pointer value will change after reuseport_grow().
    Instead of saving all checking conditions like the ones
    preced calling "reuseport_add_sock()" and compare them everytime a
    bpf_prog is run, a 32bits ID is introduced to survive the
    reuseport_grow().  The ID is only acquired if any of the
    reuse->socks[] is added to the newly introduced
    "BPF_MAP_TYPE_REUSEPORT_ARRAY" map.
    
    If "BPF_MAP_TYPE_REUSEPORT_ARRAY" is not used,  the changes in this
    patch is a no-op.
    
    Change-Id: Ic36af8f99f4f079340c6186e6a2a083ffd47dfb2
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/net/sock_reuseport.h
+++ include/net/sock_reuseport.h
@@ -5,13 +5,18 @@
 #include <linux/filter.h>
 #include <linux/skbuff.h>
 #include <linux/types.h>
+#include <linux/spinlock.h>
 #include <net/sock.h>
+
+extern spinlock_t reuseport_lock;
 
 struct sock_reuseport {
 	struct rcu_head		rcu;
 
 	u16			max_socks;	/* length of socks */
 	u16			num_socks;	/* elements in socks */
+	/* ID stays the same even after the size of socks[] grows. */
+	unsigned int		reuseport_id;
 	struct bpf_prog __rcu	*prog;		/* optional BPF sock selector */
 	struct sock		*socks[0];	/* array of sock pointers */
 };
@@ -25,5 +30,6 @@ extern struct sock *reuseport_select_sock(struct sock *sk,
 					  int hdr_len);
 extern struct bpf_prog *reuseport_attach_prog(struct sock *sk,
 					      struct bpf_prog *prog);
+int reuseport_get_id(struct sock_reuseport *reuse);
 
 #endif  /* _SOCK_REUSEPORT_H */
