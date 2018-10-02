fd1c12f75590efc038a19d75f55e82458a4c7f77
Joe Stringer <joe@wand.net.nz>
2018-10-02 13:35:35 -0700

    bpf: Add reference tracking to verifier
    Allow helper functions to acquire a reference and return it into a
    register. Specific pointer types such as the PTR_TO_SOCKET will
    implicitly represent such a reference. The verifier must ensure that
    these references are released exactly once in each path through the
    program.
    
    To achieve this, this commit assigns an id to the pointer and tracks it
    in the 'bpf_func_state', then when the function or program exits,
    verifies that all of the acquired references have been freed. When the
    pointer is passed to a function that frees the reference, it is removed
    from the 'bpf_func_state` and all existing copies of the pointer in
    registers are marked invalid.
    
    Signed-off-by: Joe Stringer <joe@wand.net.nz>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -105,6 +105,16 @@ struct bpf_stack_state {
 	struct bpf_reg_state spilled_ptr;
 	u8 slot_type[BPF_REG_SIZE];
 };
+struct bpf_reference_state {
+	/* Track each reference created with a unique id, even if the same
+	 * instruction creates the reference multiple times (eg, via CALL).
+	 */
+	int id;
+	/* Instruction where the allocation of this reference occurred. This
+	 * is used purely to inform the user of a reference leak.
+	 */
+	int insn_idx;
+};
 
 /* state of the program:
  * type of all registers and stack info
@@ -123,7 +133,9 @@ struct bpf_func_state {
 	 * zero == main subprog
 	 */
 	u32 subprogno;
-	/* should be second to last. See copy_func_state() */
+	/* The following fields should be last. See copy_func_state() */
+	int acquired_refs;
+	struct bpf_reference_state *refs;
 	int allocated_stack;
 	struct bpf_stack_state *stack;
 };
@@ -235,11 +247,16 @@ struct bpf_verifier_env {
 	u32 subprog_cnt;
 };
 
-static inline struct bpf_reg_state *cur_regs(struct bpf_verifier_env *env)
+static inline struct bpf_func_state *cur_func(struct bpf_verifier_env *env)
 {
 	struct bpf_verifier_state *cur = env->cur_state;
 
-	return cur->frame[cur->curframe]->regs;
+	return cur->frame[cur->curframe];
+}
+
+static inline struct bpf_reg_state *cur_regs(struct bpf_verifier_env *env)
+{
+	return cur_func(env)->regs;
 }
 
 int bpf_prog_offload_verifier_prep(struct bpf_verifier_env *env);
