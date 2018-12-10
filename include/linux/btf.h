f8840a31492f805af9b27925782eb8ea753d7255
Roman Gushchin <guroan@gmail.com>
2018-12-10 15:43:01 -0800

    bpf: add bpffs pretty print for cgroup local storage maps
    Implement bpffs pretty printing for cgroup local storage maps
    (both shared and per-cpu).
    Output example (captured for tools/testing/selftests/bpf/netcnt_prog.c):
    
    Shared:
      $ cat /sys/fs/bpf/map_2
      # WARNING!! The output is for debug purpose only
      # WARNING!! The output format will change
      {4294968594,1}: {9999,1039896}
    
    Per-cpu:
      $ cat /sys/fs/bpf/map_1
      # WARNING!! The output is for debug purpose only
      # WARNING!! The output format will change
      {4294968594,1}: {
      	cpu0: {0,0,0,0,0}
      	cpu1: {0,0,0,0,0}
      	cpu2: {1,104,0,0,0}
      	cpu3: {0,0,0,0,0}
      }
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/linux/btf.h
+++ include/linux/btf.h
@@ -46,6 +46,7 @@ void btf_type_seq_show(const struct btf *btf, u32 type_id, void *obj,
 		       struct seq_file *m);
 int btf_get_fd_by_id(u32 id);
 u32 btf_id(const struct btf *btf);
+bool btf_type_is_reg_int(const struct btf_type *t, u32 expected_size);
 
 #ifdef CONFIG_BPF_SYSCALL
 const struct btf_type *btf_type_by_id(const struct btf *btf, u32 type_id);
