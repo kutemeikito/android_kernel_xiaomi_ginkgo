f83956678a2f6641d8e714e2530a91d1006171d1
msdx321 <msdx321@gmail.com>
2019-03-19 08:59:22 -0400

    power: smb-lib: Allow user to skip thermal throttling when charging
    * Current can be capped by some other sources. Don't bother with them
    for now.
    
    Change-Id: I6e9f9072ed5bb19dfc53732242bcfac4ce688448
    Signed-off-by: msdx321 <msdx321@gmail.com>
    Signed-off-by: Yousef <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    
    Conflicts:
    	drivers/power/supply/qcom/smb-lib.c
    
    adapt to new smb-lib driver
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -19,6 +19,7 @@
 #include <linux/qpnp/qpnp-revid.h>
 #include <linux/irq.h>
 #include <linux/pmic-voter.h>
+#include <linux/module.h>
 #include "smb-lib.h"
 #include "smb-reg.h"
 #include "battery.h"
@@ -38,6 +39,9 @@
 			pr_debug("%s: %s: " fmt, chg->name,	\
 				__func__, ##__VA_ARGS__);	\
 	} while (0)
+
+bool skip_thermal = false;
+module_param(skip_thermal, bool, 0644);
 
 static bool is_secure(struct smb_charger *chg, int addr)
 {
@@ -2157,6 +2161,8 @@ int smblib_set_prop_batt_status(struct smb_charger *chg,
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
 {
+	int temp_level;
+
 	if (val->intval < 0)
 		return -EINVAL;
 
@@ -2167,6 +2173,12 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 		return -EINVAL;
 
 	chg->system_temp_level = val->intval;
+
+	if (skip_thermal) {
+		temp_level = chg->system_temp_level;
+		chg->system_temp_level = 0;
+	}
+
 	/* disable parallel charge in case of system temp level */
 	vote(chg->pl_disable_votable, THERMAL_DAEMON_VOTER,
 			chg->system_temp_level ? true : false, 0);
@@ -2181,6 +2193,11 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 
 	vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
 			chg->thermal_mitigation[chg->system_temp_level]);
+
+	if (skip_thermal) {
+		chg->system_temp_level = temp_level;
+	}
+
 	return 0;
 }
 
