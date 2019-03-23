70e58f6fc24ee6e167ffda1bba84f202c2f65db4
Al Viro <viro@zeniv.linux.org.uk>
2019-03-22 23:26:22 -0400

    nsfs: unobfuscate
    1) IS_ERR(p) && PTR_ERR(p) == -E... is spelled p == ERR_PTR(-E...)
    2) yes, you can open-code do-while and sometimes there's even
    a good reason to do so.  Not in this case, though.
    
    Change-Id: I4c0cdeae3e14f702755df22e900ddafcb4d99ba8
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- fs/nsfs.c
+++ fs/nsfs.c
@@ -106,17 +106,14 @@ slow:
 void *ns_get_path_cb(struct path *path, ns_get_path_helper_t *ns_get_cb,
 		     void *private_data)
 {
-	struct ns_common *ns;
 	void *ret;
 
-again:
-	ns = ns_get_cb(private_data);
-	if (!ns)
-		return ERR_PTR(-ENOENT);
-
-	ret = __ns_get_path(path, ns);
-	if (IS_ERR(ret) && PTR_ERR(ret) == -EAGAIN)
-		goto again;
+	do {
+		struct ns_common *ns = ns_get_cb(private_data);
+		if (!ns)
+			return ERR_PTR(-ENOENT);
+		ret = __ns_get_path(path, ns);
+	} while (ret == ERR_PTR(-EAGAIN));
 	return ret;
 }
 
@@ -151,7 +148,7 @@ int open_related_ns(struct ns_common *ns,
 	if (fd < 0)
 		return fd;
 
-	while (1) {
+	do {
 		struct ns_common *relative;
 
 		relative = get_ns(ns);
@@ -161,10 +158,8 @@ int open_related_ns(struct ns_common *ns,
 		}
 
 		err = __ns_get_path(&path, relative);
-		if (IS_ERR(err) && PTR_ERR(err) == -EAGAIN)
-			continue;
-		break;
-	}
+	} while (err == ERR_PTR(-EAGAIN));
+
 	if (IS_ERR(err)) {
 		put_unused_fd(fd);
 		return PTR_ERR(err);
