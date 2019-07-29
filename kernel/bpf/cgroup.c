7954472e541d514729254446497277f339a8da38
Stanislav Fomichev <sdf@google.com>
2019-07-29 14:51:10 -0700

    bpf: always allocate at least 16 bytes for setsockopt hook
    Since we always allocate memory, allocate just a little bit more
    for the BPF program in case it need to override user input with
    bigger value. The canonical example is TCP_CONGESTION where
    input string might be too small to override (nv -> bbr or cubic).
    
    16 bytes are chosen to match the size of TCP_CA_NAME_MAX and can
    be extended in the future if needed.
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -768,7 +768,6 @@ static int sockopt_alloc_buf(struct bpf_sockopt_kern *ctx, int max_optlen)
 		return -ENOMEM;
 
 	ctx->optval_end = ctx->optval + max_optlen;
-	ctx->optlen = max_optlen;
 	return 0;
 }
 
@@ -787,7 +786,7 @@ int __cgroup_bpf_run_filter_setsockopt(struct sock *sk, int *level,
 		.level = *level,
 		.optname = *optname,
 	};
-	int ret;
+	int ret, max_optlen;
 
 	/* Opportunistic check to see whether we have any BPF program
 	 * attached to the hook so we don't waste time allocating
@@ -797,9 +796,17 @@ int __cgroup_bpf_run_filter_setsockopt(struct sock *sk, int *level,
 	    __cgroup_bpf_prog_array_is_empty(cgrp, BPF_CGROUP_SETSOCKOPT))
 		return 0;
 
-	ret = sockopt_alloc_buf(&ctx, *optlen);
+	/* Allocate a bit more than the initial user buffer for
+	 * BPF program. The canonical use case is overriding
+	 * TCP_CONGESTION(nv) to TCP_CONGESTION(cubic).
+	 */
+	max_optlen = max_t(int, 16, *optlen);
+	ret = sockopt_alloc_buf(&ctx, max_optlen);
+
 	if (ret)
 		return ret;
+
+	ctx.optlen = *optlen;
 
 	if (copy_from_user(ctx.optval, optval, *optlen) != 0) {
 		ret = -EFAULT;
@@ -817,7 +824,7 @@ int __cgroup_bpf_run_filter_setsockopt(struct sock *sk, int *level,
 	if (ctx.optlen == -1) {
 		/* optlen set to -1, bypass kernel */
 		ret = 1;
-	} else if (ctx.optlen > *optlen || ctx.optlen < -1) {
+	} else if (ctx.optlen > max_optlen || ctx.optlen < -1) {
 		/* optlen is out of bounds */
 		ret = -EFAULT;
 	} else {
@@ -861,6 +868,8 @@ int __cgroup_bpf_run_filter_getsockopt(struct sock *sk, int level,
 	ret = sockopt_alloc_buf(&ctx, max_optlen);
 	if (ret)
 		return ret;
+
+	ctx.optlen = max_optlen;
 
 	if (!retval) {
 		/* If kernel getsockopt finished successfully,
