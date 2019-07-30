d357b0feee25b8d6caf068e92b381c115c9affa5
Danny Lin <danny@kdrag0n.dev>
2019-07-30 00:48:51 -0700

    sched/fair: Fix compile errors when WALT is disabled
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7805,7 +7805,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		if (!prefer_idle && !boosted &&
 			(target_cpu != -1 || best_idle_cpu != -1) &&
 			(fbt_env->placement_boost == SCHED_BOOST_NONE ||
+#ifdef CONFIG_SCHED_WALT
 			sched_boost() != FULL_THROTTLE_BOOST ||
+#endif
 			(fbt_env->placement_boost == SCHED_BOOST_ON_BIG &&
 				!next_group_higher_cap)))
 			break;
@@ -9392,9 +9394,11 @@ redo:
 
 		continue;
 next:
+#ifdef CONFIG_SCHED_WALT
 		trace_sched_load_balance_skip_tasks(env->src_cpu, env->dst_cpu,
 				env->src_grp_type, p->pid, load, task_util(p),
 				cpumask_bits(&p->cpus_allowed)[0]);
+#endif
 		list_move_tail(&p->se.group_node, tasks);
 	}
 
