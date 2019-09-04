463bc938804802f643cc033997a31e3902396d53
Sami Tolvanen <samitolvanen@google.com>
2019-09-04 14:08:16 -0700

    ANDROID: bpf: validate bpf_func when BPF_JIT is enabled with CFI
    With CONFIG_BPF_JIT, the kernel makes indirect calls to dynamically
    generated code, which the compile-time Control-Flow Integrity (CFI)
    checking cannot validate. This change adds basic sanity checking to
    ensure we are jumping to a valid location, which narrows down the
    attack surface on the stored pointer.
    
    In addition, this change adds a weak arch_bpf_jit_check_func function,
    which architectures that implement BPF JIT can override to perform
    additional validation, such as verifying that the pointer points to
    the correct memory region.
    
    Bug: 140377409
    Change-Id: I8ebac6637ab6bd9db44716b1c742add267298669
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: Edwiin kusuma jaya <kutemeikito0905@gmail.com>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -578,6 +578,7 @@ static void bpf_jit_uncharge_modmem(u32 pages)
 	atomic_long_sub(pages, &bpf_jit_current);
 }
 
+
 void *__weak bpf_jit_alloc_exec(unsigned long size)
 {
 	return module_alloc(size);
@@ -587,6 +588,14 @@ void __weak bpf_jit_free_exec(void *addr)
 {
 	module_memfree(addr);
 }
+
+#if IS_ENABLED(CONFIG_BPF_JIT) && IS_ENABLED(CONFIG_CFI_CLANG)
+bool __weak arch_bpf_jit_check_func(const struct bpf_prog *prog)
+{
+	return true;
+}
+EXPORT_SYMBOL(arch_bpf_jit_check_func);
+#endif
 
 struct bpf_binary_header *
 bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
