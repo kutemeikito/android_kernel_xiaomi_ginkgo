42edb90a4ddb45694de52f7724b994779c409da3
Khusika Dhamar Gusti <mail@khusika.com>
2019-09-13 23:05:32 +0700

    input: touchscreen: nt36xxx_spi_c3j: fix snprintf error
    Fixes the following clang error:
    
    drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx_mp_ctrlram.c:1240:3:
    warning: 'snprintf' size argument is too large; destination buffer
    has size 32, but size argument is 4096 [-Wfortify-source]
     1240 |                 snprintf(mpcriteria, PAGE_SIZE, "novatek-mp-criteria-%04X", ts->nvt_pid);
          |                 ^
    
    Change-Id: I3988e96941df236a879d64c09788d7074ded9f01
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx_mp_ctrlram.c
+++ drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx_mp_ctrlram.c
@@ -1237,7 +1237,7 @@ static int32_t nvt_selftest_open(struct inode *inode, struct file *file)
 		 * Ex. nvt_pid = 500A
 		 *     mpcriteria = "novatek-mp-criteria-500A"
 		 */
-		snprintf(mpcriteria, PAGE_SIZE, "novatek-mp-criteria-%04X", ts->nvt_pid);
+		snprintf(mpcriteria, sizeof(mpcriteria), "novatek-mp-criteria-%04X", ts->nvt_pid);
 
 		if (nvt_mp_parse_dt(np, mpcriteria)) {
 			//---Download Normal FW---
