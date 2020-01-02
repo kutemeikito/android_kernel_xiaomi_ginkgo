30059711c9bcb7e52960e4a8b8832ce0205d37c5
Yaroslav Furman <yaro330@gmail.com>
2020-01-02 16:23:00 +0300

    qcacld-3.0: do not compile logging
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -21349,9 +21349,9 @@ int wlan_hdd_disconnect(struct hdd_adapter *adapter, u16 reason)
  * Return: string conversion of reason code, if match found;
  *         "Unknown" otherwise.
  */
-#ifdef WLAN_DEBUG
 static const char *hdd_ieee80211_reason_code_to_str(uint16_t reason)
 {
+#ifdef WLAN_DEBUG
 	switch (reason) {
 	CASE_RETURN_STRING(WLAN_REASON_UNSPECIFIED);
 	CASE_RETURN_STRING(WLAN_REASON_PREV_AUTH_NOT_VALID);
@@ -21403,8 +21403,9 @@ static const char *hdd_ieee80211_reason_code_to_str(uint16_t reason)
 	default:
 		return "Unknown";
 	}
-}
 #endif
+	return "";
+}
 
 /**
  * hdd_print_netdev_txq_status() - print netdev tx queue status
