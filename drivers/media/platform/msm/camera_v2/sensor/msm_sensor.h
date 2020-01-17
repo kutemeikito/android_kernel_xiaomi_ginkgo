e5b4952079b3a3af874c37905e94aa7eefe71451
Rama Bondan Prakoso <ramanarubp@gmail.com>
2020-01-17 13:01:16 +0700

    Revert "msm: camera/arch: arm64: Update to latest camera changes"
    This reverts commit a39d337080d4da0ab0ea18e243ba0529a0595478.

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2011-2016, 2018, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -90,7 +91,6 @@ struct msm_sensor_ctrl_t {
 	uint8_t is_csid_tg_mode;
 	uint32_t is_secure;
 	uint8_t bypass_video_node_creation;
-	uint8_t is_virtual_camera;
 };
 
 int msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl, void *argp);
