116ab177eedf6a7ecdf00b7c7ff9ac5be2b8d50c
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-01-31 10:14:14 +0700

    usb: Import ginkgo changes
    * based on ginkgo-p-oss
    * cleanup code styles
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -55,6 +56,10 @@
 #include "debug.h"
 #include "xhci.h"
 
+#undef dev_dbg
+#define dev_dbg dev_err
+#undef pr_debug
+#define pr_debug pr_info
 #define SDP_CONNETION_CHECK_TIME 10000 /* in ms */
 
 /* time out to wait for USB cable status notification (in ms)*/
@@ -69,6 +74,9 @@
 /* XHCI registers */
 #define USB3_HCSPARAMS1		(0x4)
 #define USB3_PORTSC		(0x420)
+
+#define DWC3_DCTL	0xc704
+#define DWC3_DCTL_RUN_STOP	BIT(31)
 
 /**
  *  USB QSCRATCH Hardware registers
@@ -4631,6 +4639,16 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 				atomic_read(&mdwc->dev->power.usage_count));
 			dwc3_otg_start_peripheral(mdwc, 1);
 			mdwc->drd_state = DRD_STATE_PERIPHERAL;
+
+			if (!dwc->softconnect && get_psy_type(mdwc) == POWER_SUPPLY_TYPE_USB_CDP) {
+				u32 reg;
+
+				dbg_event(0xFF, "cdp pullup dp", 0);
+				reg = dwc3_readl(dwc->regs, DWC3_DCTL);
+				reg |= DWC3_DCTL_RUN_STOP;
+				dwc3_writel(dwc->regs, DWC3_DCTL, reg);
+				break;
+			}
 			work = 1;
 		} else {
 			dwc3_msm_gadget_vbus_draw(mdwc, 0);
--- drivers/usb/phy/phy-msm-qusb.c
+++ drivers/usb/phy/phy-msm-qusb.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2019 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -26,6 +27,10 @@
 #include <linux/regulator/machine.h>
 #include <linux/usb/phy.h>
 #include <linux/reset.h>
+#undef dev_dbg
+#define dev_dbg dev_err
+#undef pr_debug
+#define pr_debug pr_info
 
 #define QUSB2PHY_PLL_PWR_CTL		0x18
 #define REF_BUF_EN			BIT(0)
@@ -134,6 +139,7 @@ MODULE_PARM_DESC(tune5, "QUSB PHY TUNE5");
 static bool eud_connected;
 module_param(eud_connected, bool, 0644);
 MODULE_PARM_DESC(eud_connected, "EUD_CONNECTED");
+unsigned long panel_info = 0;
 
 struct qusb_phy {
 	struct usb_phy		phy;
@@ -405,7 +411,17 @@ static void qusb_phy_get_tune2_param(struct qusb_phy *qphy)
 	pr_debug("%s(): efuse based tune2 value:%d\n",
 				__func__, qphy->tune2_val);
 
-	/* Update higher nibble of TUNE2 value for better rise/fall times */
+	/*
+	 * Update higher nibble of TUNE2 value for better rise/fall times
+	 * add for different usb tuning parameters judged by panal info
+	 */
+	if (panel_info == 1)
+		qphy->tune2_efuse_correction = -1;
+	else if (panel_info == 0)
+		qphy->tune2_efuse_correction = 3;
+	else
+		qphy->tune2_efuse_correction = 0;
+
 	if (qphy->tune2_efuse_correction && qphy->tune2_val) {
 		if (qphy->tune2_efuse_correction > 5 ||
 				qphy->tune2_efuse_correction < -10)
@@ -1257,7 +1273,12 @@ static int qusb_phy_probe(struct platform_device *pdev)
 	}
 
 	size = 0;
-	of_get_property(dev->of_node, "qcom,qusb-phy-init-seq", &size);
+	pr_info("panel_info %x\n", panel_info);
+	if (panel_info == 1)
+		of_get_property(dev->of_node, "qcom,qusb-phy-init-seq", &size);
+	else if (panel_info == 0)
+		of_get_property(dev->of_node, "qcom,qusb-phy-init-seq-no-panel", &size);
+
 	if (size) {
 		qphy->qusb_phy_init_seq = devm_kzalloc(dev,
 						size, GFP_KERNEL);
@@ -1269,10 +1290,16 @@ static int qusb_phy_probe(struct platform_device *pdev)
 				return -EINVAL;
 			}
 
-			of_property_read_u32_array(dev->of_node,
-				"qcom,qusb-phy-init-seq",
-				qphy->qusb_phy_init_seq,
-				qphy->init_seq_len);
+			if (panel_info == 1)
+				of_property_read_u32_array(dev->of_node,
+					"qcom,qusb-phy-init-seq",
+					qphy->qusb_phy_init_seq,
+					qphy->init_seq_len);
+			else if (panel_info == 0)
+				of_property_read_u32_array(dev->of_node,
+					"qcom,qusb-phy-init-seq-no-panel",
+					qphy->qusb_phy_init_seq,
+					qphy->init_seq_len);
 		} else {
 			dev_err(dev, "error allocating memory for phy_init_seq\n");
 		}
@@ -1383,6 +1410,20 @@ static int qusb_phy_remove(struct platform_device *pdev)
 
 	return 0;
 }
+
+static int __init parameter_select(char *str)
+{
+	int ret = 0;
+
+	ret = kstrtol(str, 10, &panel_info);
+	if (ret < 0)
+		return ret;
+	if (panel_info > 1)
+		pr_err("can't get panel_info\n");
+	pr_info("get panel_info %x from cmdline\n", panel_info);
+	return 1;
+}
+__setup("panel_info=", parameter_select);
 
 static const struct of_device_id qusb_phy_id_table[] = {
 	{ .compatible = "qcom,qusb2phy", },
