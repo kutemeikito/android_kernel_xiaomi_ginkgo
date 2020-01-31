a8128942963ba2fc80460ae6f1f6ecbfbe41c008
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-01-31 09:49:40 +0700

    power: Import ginkgo changes
    * based on ginkgo-p-oss
    * cleanup code styles
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -45,6 +45,8 @@
 #include <linux/notifier.h>
 #endif
 
+#include <linux/wakeup_reason.h> /*Add-HMI_M516_A01-51*/
+
 #define MAX_NR_GPIO 300
 #define PS_HOLD_OFFSET 0x820
 
@@ -621,8 +623,20 @@ static void msm_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 	unsigned i;
 
 	for (i = 0; i < chip->ngpio; i++, gpio++) {
+/*Add-begin-HMI_M516_A01-51
+**Author:lijiang@longcheer.com
+**Date:2019-4-22
+**Comment:remove tz gpio
+*/
+		if (i != 30 && i != 31 && i != 32 && i != 33 &&
+		    i != 0 && i != 1 && i != 2 && i != 3) {
+			msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
+			seq_puts(s, "\n");
+		}
+/*
 		msm_gpio_dbg_show_one(s, NULL, chip, i, gpio);
 		seq_puts(s, "\n");
+*/
 	}
 }
 
@@ -1980,6 +1994,9 @@ static void msm_pinctrl_resume(void)
 				name = desc->action->name;
 
 			pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+
+			log_wakeup_reason(irq); /*Add-HMI_M516_A01-51*/
+
 		}
 	}
 	raw_spin_unlock_irqrestore(&pctrl->lock, flags);
@@ -2093,4 +2110,3 @@ int msm_pinctrl_remove(struct platform_device *pdev)
 	return 0;
 }
 EXPORT_SYMBOL(msm_pinctrl_remove);
-
