2bf40de5dd60337adfff60542e6b5badc8da95f9
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-02-14 07:55:39 +0700

    power: smb5-lib: fix misleading-indentation
    a fix for 57c6daa3 ("power: Import ginkgo changes")
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -1027,9 +1027,10 @@ static const struct apsd_result *smblib_update_usb_type(struct smb_charger *chg)
 		 * detected as as SDP
 		 */
 		if (!(apsd_result->pst == POWER_SUPPLY_TYPE_USB_FLOAT &&
-			chg->real_charger_type == POWER_SUPPLY_TYPE_USB))
+			chg->real_charger_type == POWER_SUPPLY_TYPE_USB)) {
 			chg->real_charger_type = apsd_result->pst;
 			chg->usb_psy_desc.type = apsd_result->pst;
+		}
 	}
 
 	smblib_err(chg,"APSD=%s PD=%d\n",apsd_result->name, chg->pd_active);
