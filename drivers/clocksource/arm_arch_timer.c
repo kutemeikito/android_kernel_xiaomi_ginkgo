7c89a9fbdc55c7dc2a74bf0dcfabeeb1286eb368
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-02-15 13:36:19 +0700

    treewide: silence some logging
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -791,7 +791,7 @@ static void arch_counter_set_user_access(void)
 	 * disabled though.
 	 */
 	if (arch_timer_this_cpu_has_cntvct_wa())
-		pr_info("CPU%d: Trapping CNTVCT access\n", smp_processor_id());
+		pr_debug("CPU%d: Trapping CNTVCT access\n", smp_processor_id());
 	else if (IS_ENABLED(CONFIG_ARM_ARCH_TIMER_VCT_ACCESS))
 		cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN;
 
