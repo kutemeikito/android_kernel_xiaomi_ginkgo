b5bfa4cd8e0f99962059358397ba981ed5eebbf6
uvera <dusan.uveric9@gmail.com>
2020-04-01 18:37:46 +0200

    msm/camera_v2: Boost CPU and DDR for 100ms on camera focus
    Signed-off-by: Adithya R <radithya2002@gmail.com>
    Change-Id: If2a604cd94e668853508b65f335e4c5eb83cf3b6

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -13,6 +13,8 @@
 #define pr_fmt(fmt) "%s:%d " fmt, __func__, __LINE__
 
 #include <linux/module.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include "msm_sd.h"
 #include "msm_actuator.h"
 #include "msm_cci.h"
@@ -531,6 +533,9 @@ static int32_t msm_actuator_piezo_move_focus(
 
 	CDBG("Enter\n");
 
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+
 	if (a_ctrl->i2c_reg_tbl == NULL) {
 		pr_err("failed. i2c reg table is NULL");
 		return -EFAULT;
@@ -593,6 +598,9 @@ static int32_t msm_actuator_move_focus(
 	struct msm_camera_i2c_reg_setting reg_setting;
 
 	CDBG("called, dir %d, num_steps %d\n", dir, num_steps);
+
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	if ((dest_step_pos == a_ctrl->curr_step_pos) ||
 		((dest_step_pos <= a_ctrl->total_steps) &&
@@ -722,6 +730,9 @@ static int32_t msm_actuator_bivcm_move_focus(
 	}
 
 	CDBG("called, dir %d, num_steps %d\n", dir, num_steps);
+
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	if (dest_step_pos == a_ctrl->curr_step_pos)
 		return rc;
