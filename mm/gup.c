4ae55366984630968995f64c158d9b36aff95e9a
Peter Xu <peterx@redhat.com>
2020-04-07 21:40:10 -0400

    UPSTREAM: mm/gup: Mark lock taken only after a successful retake
    It's definitely incorrect to mark the lock as taken even if
    down_read_killable() failed.
    
    This wass overlooked when we switched from down_read() to
    down_read_killable() because down_read() won't fail while
    down_read_killable() could.
    
    Fixes: 71335f37c5e8 ("mm/gup: allow to react to fatal signals")
    Reported-by: syzbot+a8c70b7f3579fc0587dc@syzkaller.appspotmail.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit c7b6a566b98524baea6a244186e665d22b633545)
    
    Bug: 176847924
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Change-Id: Id0e1fe147e87ff6bba02d5e590f6346089c5e650
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- mm/gup.c
+++ mm/gup.c
@@ -979,7 +979,6 @@ retry:
 		if (fatal_signal_pending(current))
 			break;
 
-		*locked = 1;
 		ret = down_read_killable(&mm->mmap_sem);
 		if (ret) {
 			BUG_ON(ret > 0);
@@ -988,6 +987,7 @@ retry:
 			break;
 		}
 
+		*locked = 1;
 		ret = __get_user_pages(tsk, mm, start, 1, flags | FOLL_TRIED,
 				       pages, NULL, locked);
 		if (!*locked) {
