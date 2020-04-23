a4db84de2af50772d50063a093b3280753956007
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-04-24 05:17:26 +0700

    qca-wifi-host-cmn: Fix build after LA.UM.8.1.r1-15100-sm8150.0 merge
    CONFIG_WMI_INTERFACE_EVENT_LOGGING is disabled in commit
    86f2a8f3 ("qcacld: disable bunch of more debug configs")
    this wmi_hang_event added by caf in d7ec6d5a ("qcacmn: update the hang
    data for wmi") depend on CONFIG_WMI_INTERFACE_EVENT_LOGGING,
    add ifdef to fix build when CONFIG_WMI_INTERFACE_EVENT_LOGGING is
    disabled
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Change-Id: I6f1298ea65173fa4d7723745558d9f1d62b3e222
    Signed-off-by: Adithya R <radithya2002@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/wmi/src/wmi_unified.c
+++ drivers/staging/qca-wifi-host-cmn/wmi/src/wmi_unified.c
@@ -2377,8 +2377,9 @@ void *wmi_unified_attach(void *scn_handle,
 
 	wmi_wbuff_register(wmi_handle);
 
+#ifdef WMI_INTERFACE_EVENT_LOGGING
 	wmi_hang_event_notifier_register(wmi_handle);
-
+#endif
 	return wmi_handle;
 
 error:
@@ -2401,7 +2402,9 @@ void wmi_unified_detach(struct wmi_unified *wmi_handle)
 	struct wmi_soc *soc;
 	uint8_t i;
 
+#ifdef WMI_INTERFACE_EVENT_LOGGING
 	wmi_hang_event_notifier_unregister();
+#endif
 
 	wmi_wbuff_deregister(wmi_handle);
 
