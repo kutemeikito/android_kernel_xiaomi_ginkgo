18c94cbef331ca9042134abfda501d6c07f8a2a1
billaids <jimmy.nelle@hsw-stud.de>
2020-04-26 18:31:41 +0200

    Revert "drm: msm: kcal: make the userspace module param vars static and use short instead of integers"
    This reverts commit 4e60446ffa4a7719182c930fed60150fc09c7103.
    
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -14,22 +14,6 @@
 #include "sde_hw_color_proc_common_v4.h"
 #include "sde_hw_color_proc_v4.h"
 
-static unsigned short kcal_red = 256;
-static unsigned short kcal_green = 256;
-static unsigned short kcal_blue = 256;
-static unsigned short kcal_hue = 0;
-static unsigned short kcal_sat = 255;
-static unsigned short kcal_val = 255;
-static unsigned short kcal_cont = 255;
-
-module_param(kcal_red, short, 0644);
-module_param(kcal_green, short, 0644);
-module_param(kcal_blue, short, 0644);
-module_param(kcal_hue, short, 0644);
-module_param(kcal_sat, short, 0644);
-module_param(kcal_val, short, 0644);
-module_param(kcal_cont, short, 0644);
-
 static int sde_write_3d_gamut(struct sde_hw_blk_reg_map *hw,
 		struct drm_msm_3d_gamut *payload, u32 base,
 		u32 *opcode, u32 pipe, u32 scale_tbl_a_len,
