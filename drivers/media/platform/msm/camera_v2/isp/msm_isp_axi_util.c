6a954c429670fd6b031c50ccf7fd6028793f9ff8
dianlujitao <dianlujitao@lineageos.org>
2020-05-08 17:51:21 +0800

    msm: camera_v2: conditionality eliminate drop_reconfig flag
    The flag is set to notify userspace driver, and expected to be clear
    there. However, our userspace driver isn't able to handle this, as a
    result the camera stopped working once it's set because an empty frame
    is always returned.
    
    This conditionality reverts bc82952
    and 39e7df3
    
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -3765,6 +3765,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		) {
 		pr_debug("%s:%d invalid time to request frame %d try drop_reconfig\n",
 			__func__, __LINE__, frame_id);
+#if 1
+		goto error;
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
 		return 0;
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active) &&
@@ -3779,6 +3782,7 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].frame_id,
 			vfe_dev->axi_data.src_info[VFE_PIX_0].active);
 		return 0;
+#endif
 	} else if ((vfe_dev->axi_data.src_info[frame_src].active && (frame_id !=
 		vfe_dev->axi_data.src_info[frame_src].frame_id +
 		vfe_dev->axi_data.src_info[frame_src].sof_counter_step)) ||
@@ -3810,7 +3814,18 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			__func__, __LINE__, vfe_dev->pdev->id, frame_id,
 			stream_info->activated_framedrop_period,
 			stream_info->stream_id);
+#if 1
+		rc = msm_isp_return_empty_buffer(vfe_dev, stream_info,
+			user_stream_id, frame_id, buf_index, frame_src);
+		if (rc < 0)
+			pr_err("%s:%d failed: return_empty_buffer src %d\n",
+				__func__, __LINE__, frame_src);
+		stream_info->current_framedrop_period =
+			MSM_VFE_STREAM_STOP_PERIOD;
+		msm_isp_cfg_framedrop_reg(stream_info);
+#else
 		vfe_dev->isp_page->drop_reconfig = 1;
+#endif
 		return 0;
 	}
 
