d1121a6e3dc855fb6368fd8e5b42fae49d0dce04
Kristof Petho <kristof.petho@gmail.com>
2020-05-24 09:34:41 +0200

    cnss2: Omit debug logging code

--- drivers/net/wireless/cnss2/debug.h
+++ drivers/net/wireless/cnss2/debug.h
@@ -26,23 +26,19 @@ extern void *cnss_ipc_log_context;
 	} while (0)
 
 #define cnss_pr_err(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_ERR, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
+		no_printk(_fmt, ##__VA_ARGS__);				\
 	} while (0)
 
 #define cnss_pr_warn(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_WARNING, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
+		no_printk(_fmt, ##__VA_ARGS__);					\
 	} while (0)
 
 #define cnss_pr_info(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_INFO, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
+		no_printk(_fmt, ##__VA_ARGS__);					\
 	} while (0)
 
 #define cnss_pr_dbg(_fmt, ...) do {					\
-		printk("%scnss: " _fmt, KERN_DEBUG, ##__VA_ARGS__);	\
-		cnss_ipc_log_string("%scnss: " _fmt, "", ##__VA_ARGS__);\
+		no_printk(_fmt, ##__VA_ARGS__);				\
 	} while (0)
 
 #ifdef CONFIG_CNSS2_DEBUG
