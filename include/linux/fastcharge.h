6ed3894dd58d207cb0efa439731333fda8e6b7ea
Kyvangka1610 <kyvangka2002@gmail.com>
2020-06-20 09:54:44 +0700

    Fix FastCharge Issue
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

---
+++ include/linux/fastcharge.h
@@ -0,0 +1,11 @@
+
+/*
+ *
+ * USB Fastcharge
+ *
+ *
+*/
+
+#define USB_FASTCHARGE_CURRENT_LIMIT 2000
+
+extern int force_fast_charge;
--- include/linux/fastchg.h
+++ include/linux/fastchg.h
@@ -1,7 +1,7 @@
 /*
  * Author: Chad Froebel <chadfroebel@gmail.com>
  *
- * Port to guacamole: engstk <eng.stk@sapo.pt>
+ * Port to cheeseburger: engstk <eng.stk@sapo.pt>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -18,8 +18,4 @@
 #define _LINUX_FASTCHG_H
 
 extern int get_force_fast_charge(void);
-
-extern int force_fast_charge;
-
 #endif
-
