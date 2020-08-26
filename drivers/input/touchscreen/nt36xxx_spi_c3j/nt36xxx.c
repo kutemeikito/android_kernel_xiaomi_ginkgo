62db55eab24affd5e09ce65c601bbc14598f3bb8
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-08-26 19:09:45 +0700

    input/ts: nt36xxx: Require low latency
    Currently, CPU can enter deep sleep while waiting for data on the i2c
    bug. That means an i2c read of 8 bytes can take as long as 10 ms, which
    would overrun the desired interrupt handling time.
    Use pm qos to require low latency and prevent CPU from entering deep
    sleep while handling touch interrupts.
    
    Also optimize it by only waking up a core that's servicing and IRQ.
    
    Ref https://github.com/aosp-mirror/kernel_msm/commit/c775f94bab51bfd1820fa0ff4be9a584ee6d34b0
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I59039c4db19f04af1ee433b975f2449ab751e6c4
    Signed-off-by: HafizZiq <hafizuddinismail552@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx.c
@@ -1351,6 +1351,8 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	int32_t i = 0;
 	int32_t finger_cnt = 0;
 
+	pm_qos_update_request(&ts->pm_qos_req, 100);
+
 #if WAKEUP_GESTURE
 #ifdef CONFIG_PM
 	if (ts->dev_pm_suspend && ts->is_gesture_mode) {
@@ -1401,8 +1403,7 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	if (bTouchIsAwake == 0) {
 		input_id = (uint8_t)(point_data[1] >> 3);
 		nvt_ts_wakeup_gesture_report(input_id, point_data);
-		mutex_unlock(&ts->lock);
-		return IRQ_HANDLED;
+		goto XFER_ERROR;
 	}
 #endif
 
@@ -1498,6 +1499,7 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 	input_sync(ts->input_dev);
 
 XFER_ERROR:
+	pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
 
 	mutex_unlock(&ts->lock);
 
@@ -2059,6 +2061,9 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 			nvt_irq_enable(false);
 			NVT_LOG("request irq %d succeed\n", client->irq);
 		}
+
+		pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+				PM_QOS_DEFAULT_VALUE);
 	}
 
 #if WAKEUP_GESTURE
@@ -2352,6 +2357,8 @@ static int32_t nvt_ts_remove(struct spi_device *client)
 	uninit_lct_tp_gesture();
 #endif
 	uninit_lct_tp_info();
+
+	pm_qos_remove_request(&ts->pm_qos_req);
 
 #if NVT_TOUCH_MP
 	nvt_mp_proc_deinit();
