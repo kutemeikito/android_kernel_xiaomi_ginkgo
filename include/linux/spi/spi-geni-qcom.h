ce38d8c9c7dac7d2678edc304f609829f8086a4f
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-05 17:17:02 -0800

    [REAPPLY]spi-geni-qcom: Add a function to get the IRQ of a device's master
    Exporting the IRQ of a SPI device's master controller can help device
    drivers utilize the PM QoS API to force the SPI master IRQ to be
    serviced with low latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Change-Id: I4d7c597b390438c6bda97d94062794bf08c30b21
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/linux/spi/spi-geni-qcom.h
+++ include/linux/spi/spi-geni-qcom.h
@@ -29,4 +29,7 @@ struct spi_geni_qcom_ctrl_data {
 struct device *lct_get_spi_geni_master_dev(struct spi_master *spi);
 #endif
 
+struct spi_device;
+int geni_spi_get_master_irq(struct spi_device *spi_slv);
+
 #endif /*__SPI_GENI_QCOM_HEADER___*/
