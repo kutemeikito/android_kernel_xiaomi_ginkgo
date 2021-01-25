9b1a977d02c50b063acedc2a6ad3889588f19d14
Alessio Balsini <balsini@google.com>
2021-01-25 17:01:30 +0000

    FROMLIST: fuse: 32-bit user space ioctl compat for fuse device
    With a 64-bit kernel build the FUSE device cannot handle ioctl requests
    coming from 32-bit user space.
    This is due to the ioctl command translation that generates different
    command identifiers that thus cannot be used for direct comparisons
    without proper manipulation.
    
    Explicitly extract type and number from the ioctl command to enable
    32-bit user space compatibility on 64-bit kernel builds.
    
    Bug: 179164095
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-3-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: I595517c54d551be70e83c7fcb4b62397a3615004
    Signed-off-by: Alessio Balsini <balsini@google.com>
    (cherry picked from commit af4048924e191bda0bb85b4bf127f22cf3c70fba)
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -792,8 +792,6 @@ struct fuse_notify_retrieve_in {
 /* Device ioctls: */
 #define FUSE_DEV_IOC_MAGIC		229
 #define FUSE_DEV_IOC_CLONE		_IOR(FUSE_DEV_IOC_MAGIC, 0, uint32_t)
-/* 127 is reserved for the V1 interface implementation in Android */
-#define FUSE_DEV_IOC_PASSTHROUGH_OPEN	_IOW(FUSE_DEV_IOC_MAGIC, 127, struct fuse_passthrough_out)
 
 struct fuse_lseek_in {
 	uint64_t	fh;
