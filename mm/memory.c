a1f51cc2036ce2fc3023ed18925738971b141f6f
Nadav Amit <namit@vmware.com>
2021-03-12 21:08:17 -0800

    UPSTREAM: mm/userfaultfd: fix memory corruption due to writeprotect
    Userfaultfd self-test fails occasionally, indicating a memory corruption.
    
    Analyzing this problem indicates that there is a real bug since mmap_lock
    is only taken for read in mwriteprotect_range() and defers flushes, and
    since there is insufficient consideration of concurrent deferred TLB
    flushes in wp_page_copy().  Although the PTE is flushed from the TLBs in
    wp_page_copy(), this flush takes place after the copy has already been
    performed, and therefore changes of the page are possible between the time
    of the copy and the time in which the PTE is flushed.
    
    To make matters worse, memory-unprotection using userfaultfd also poses a
    problem.  Although memory unprotection is logically a promotion of PTE
    permissions, and therefore should not require a TLB flush, the current
    userrfaultfd code might actually cause a demotion of the architectural PTE
    permission: when userfaultfd_writeprotect() unprotects memory region, it
    unintentionally *clears* the RW-bit if it was already set.  Note that this
    unprotecting a PTE that is not write-protected is a valid use-case: the
    userfaultfd monitor might ask to unprotect a region that holds both
    write-protected and write-unprotected PTEs.
    
    The scenario that happens in selftests/vm/userfaultfd is as follows:
    
    cpu0				cpu1			cpu2
    ----				----			----
    							[ Writable PTE
    							  cached in TLB ]
    userfaultfd_writeprotect()
    [ write-*unprotect* ]
    mwriteprotect_range()
    mmap_read_lock()
    change_protection()
    
    change_protection_range()
    ...
    change_pte_range()
    [ *clear* “write”-bit ]
    [ defer TLB flushes ]
    				[ page-fault ]
    				...
    				wp_page_copy()
    				 cow_user_page()
    				  [ copy page ]
    							[ write to old
    							  page ]
    				...
    				 set_pte_at_notify()
    
    A similar scenario can happen:
    
    cpu0		cpu1		cpu2		cpu3
    ----		----		----		----
    						[ Writable PTE
    				  		  cached in TLB ]
    userfaultfd_writeprotect()
    [ write-protect ]
    [ deferred TLB flush ]
    		userfaultfd_writeprotect()
    		[ write-unprotect ]
    		[ deferred TLB flush]
    				[ page-fault ]
    				wp_page_copy()
    				 cow_user_page()
    				 [ copy page ]
    				 ...		[ write to page ]
    				set_pte_at_notify()
    
    This race exists since commit 292924b26024 ("userfaultfd: wp: apply
    _PAGE_UFFD_WP bit").  Yet, as Yu Zhao pointed, these races became apparent
    since commit 09854ba94c6a ("mm: do_wp_page() simplification") which made
    wp_page_copy() more likely to take place, specifically if page_count(page)
    > 1.
    
    To resolve the aforementioned races, check whether there are pending
    flushes on uffd-write-protected VMAs, and if there are, perform a flush
    before doing the COW.
    
    Further optimizations will follow to avoid during uffd-write-unprotect
    unnecassary PTE write-protection and TLB flushes.
    
    Bug: 254441685
    Link: https://lkml.kernel.org/r/20210304095423.3825684-1-namit@vmware.com
    Fixes: 09854ba94c6a ("mm: do_wp_page() simplification")
    Signed-off-by: Nadav Amit <namit@vmware.com>
    Suggested-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Tested-by: Peter Xu <peterx@redhat.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Pavel Emelyanov <xemul@openvz.org>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: <stable@vger.kernel.org>	[5.9+]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 6ce64428d62026a10cb5d80138ff2f90cc21d367)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ie35334aa739acfade88b74c9e5dde5c8a387925d
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3009,6 +3009,14 @@ static int do_wp_page(struct vm_fault *vmf)
 {
 	struct vm_area_struct *vma = vmf->vma;
 
+	/*
+	 * Userfaultfd write-protect can defer flushes. Ensure the TLB
+	 * is flushed in this case before copying.
+	 */
+	if (unlikely(userfaultfd_wp(vmf->vma) &&
+		     mm_tlb_flush_pending(vmf->vma->vm_mm)))
+		flush_tlb_page(vmf->vma, vmf->address);
+
 	vmf->page = __vm_normal_page(vma, vmf->address, vmf->orig_pte, false,
 				     vmf->vma_flags);
 	if (!vmf->page) {
