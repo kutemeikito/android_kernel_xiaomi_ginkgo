494d14f773ac51bab3ca03d2225fdc65dfe82409
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-08-23 19:44:54 +0700

    cpu_input_boost: Disable boosting when battery saver is enabled
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -12,6 +12,7 @@
 #include <linux/kthread.h>
 #include <linux/version.h>
 #include <linux/slab.h>
+#include <linux/battery_saver.h>
 #include <linux/moduleparam.h>
 #include <uapi/linux/sched/types.h>
 
@@ -109,7 +110,7 @@ static void update_online_cpu_policy(void)
 
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
-	if (test_bit(SCREEN_OFF, &b->state))
+	if (test_bit(SCREEN_OFF, &b->state) || is_battery_saver_on())
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
@@ -131,7 +132,7 @@ static void __cpu_input_boost_kick_max(struct boost_drv *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (test_bit(SCREEN_OFF, &b->state))
+	if (test_bit(SCREEN_OFF, &b->state) || is_battery_saver_on())
 		return;
 
 	do {
