68d1b7c6a8f67151c6f31f113951b1b68d13636a
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-08-30 08:40:18 +0700

    touchscreen: nt36xxx_spi_c3j: Remove MP CTRLRAM support
    Most of this is debugging or hardware testing. We really don't need to
    bother with that.
    
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx_spi_c3j/nt36xxx.c
@@ -69,11 +69,6 @@ extern int32_t nvt_extra_proc_init(void);
 extern void nvt_extra_proc_deinit(void);
 #endif
 
-#if NVT_TOUCH_MP
-extern int32_t nvt_mp_proc_init(void);
-extern void nvt_mp_proc_deinit(void);
-#endif
-
 struct nvt_ts_data *ts;
 static struct device *spi_geni_master_dev;
 #if BOOT_UPDATE_FIRMWARE
@@ -2013,14 +2008,6 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	}
 #endif
 
-#if NVT_TOUCH_MP
-	ret = nvt_mp_proc_init();
-	if (ret != 0) {
-		NVT_ERR("nvt mp proc init failed. ret=%d\n", ret);
-		goto err_mp_proc_init_failed;
-	}
-#endif
-
 	//create longcheer procfs node
 	ret = init_lct_tp_info("[Vendor]unkown,[FW]unkown,[IC]unkown\n", NULL);
 	if (ret < 0) {
@@ -2154,10 +2141,6 @@ uninit_lct_tp_gesture();
 #endif
 err_init_lct_tp_info_failed:
 uninit_lct_tp_info();
-#if NVT_TOUCH_MP
-nvt_mp_proc_deinit();
-err_mp_proc_init_failed:
-#endif
 #if NVT_TOUCH_EXT_PROC
 nvt_extra_proc_deinit();
 err_extra_proc_init_failed:
@@ -2260,9 +2243,6 @@ static int32_t nvt_ts_remove(struct spi_device *client)
 #endif
 	uninit_lct_tp_info();
 
-#if NVT_TOUCH_MP
-	nvt_mp_proc_deinit();
-#endif
 #if NVT_TOUCH_EXT_PROC
 	nvt_extra_proc_deinit();
 #endif
@@ -2356,9 +2336,6 @@ static void nvt_ts_shutdown(struct spi_device *client)
 #endif
 	uninit_lct_tp_info();
 
-#if NVT_TOUCH_MP
-	nvt_mp_proc_deinit();
-#endif
 #if NVT_TOUCH_EXT_PROC
 	nvt_extra_proc_deinit();
 #endif
