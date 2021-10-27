a3f1432066d3affc58aae3802747edfa66d1e9fc
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-10-27 17:14:30 +0700

    drivers/slimbus: slim-msm-ngd: Fix format specifier warning
    Fixes: d4c786a1f856 ("slim-msm-ngd: Check current state of sub system restart notifier")
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/slimbus/slim-msm-ngd.c
+++ drivers/slimbus/slim-msm-ngd.c
@@ -279,7 +279,7 @@ static int dsp_domr_notify_cb(struct notifier_block *n, unsigned long code,
 		break;
 	case SUBSYS_AFTER_POWERUP:
 	case SERVREG_NOTIF_SERVICE_STATE_UP_V01:
-		SLIM_INFO(dev, "SLIM DSP SSR notify cb:0x%x\n", code);
+		SLIM_INFO(dev, "SLIM DSP SSR notify cb:0x%lx\n", code);
 		atomic_set(&dev->ssr_in_progress, 0);
 		schedule_work(&dev->dsp.dom_up);
 		break;
