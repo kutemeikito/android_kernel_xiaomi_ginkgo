af26d64b28f91f24941ea45feb9094fc0c680727
Nauval Rizky <enuma.alrizky@gmail.com>
2021-11-22 08:56:47 +0700

    base: firmware: Add Xiaomi vendor firmware path
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    [fiqri19102002: Remove system firmware path]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -382,6 +382,9 @@ static void fw_free_buf(struct firmware_buf *buf)
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	"/system/vendor/firmware",
+#endif
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
