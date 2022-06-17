5de9b2b646ec9b27f6c75191125039aa1e6eb08f
Nauval Rizky <enuma.alrizky@gmail.com>
2022-06-17 12:35:46 +0700

    extcon: Allocate blocking_notifier_head after registering the device
    Commit 6e721f3ad0535b24f19a62420f4da95212cf069c
    ("extcon: Modify extcon device to be created after driver data is set")
    move device_register right before adding extcon_dev_list.
    
    This is fine on upstream, however since we have blocking notifier and
    it uses managed kzalloc (which has to be used when the dev is already
    registered successfully), we have to move it as well.
    
    Fixes: a4febd54c189fc2cb63205461bdd58e909e0fb66
    ("extcon: Add usage of blocking notifier chain")
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/extcon/extcon.c
+++ drivers/extcon/extcon.c
@@ -1098,7 +1098,7 @@ static void dummy_sysfs_dev_release(struct device *dev)
  * @supported_cable:	the array of the supported external connectors
  *			ending with EXTCON_NONE.
  *
- * Note that this function allocates the memory for extcon device 
+ * Note that this function allocates the memory for extcon device
  * and initialize default setting for the extcon device.
  *
  * Returns the pointer memory of allocated extcon_dev if success
@@ -1313,13 +1313,6 @@ int extcon_dev_register(struct extcon_dev *edev)
 		}
 	}
 
-	edev->bnh = devm_kcalloc(&edev->dev,
-			edev->max_supported, sizeof(*edev->bnh), GFP_KERNEL);
-	if (!edev->bnh) {
-		ret = -ENOMEM;
-		goto err_dev;
-	}
-
 	for (index = 0; index < edev->max_supported; index++)
 		RAW_INIT_NOTIFIER_HEAD(&edev->nh[index]);
 
@@ -1331,6 +1324,13 @@ int extcon_dev_register(struct extcon_dev *edev)
 	ret = device_register(&edev->dev);
 	if (ret) {
 		put_device(&edev->dev);
+		goto err_dev;
+	}
+
+	edev->bnh = devm_kzalloc(&edev->dev,
+			sizeof(*edev->bnh) * edev->max_supported, GFP_KERNEL);
+	if (!edev->bnh) {
+		ret = -ENOMEM;
 		goto err_dev;
 	}
 
