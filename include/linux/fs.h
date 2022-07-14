47078924597887e40907d61d110cc4d6bc094121
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-07-14 11:06:12 +0700

    fs: Remove duplicate `s_encoding` and `s_encoding_flags` members
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1398,10 +1398,6 @@ struct super_block {
 	__u16 s_encoding_flags;
 #endif
 	struct hlist_bl_head	s_anon;		/* anonymous dentries for (nfs) exporting */
-#ifdef CONFIG_UNICODE
-	struct unicode_map *s_encoding;
-	__u16 s_encoding_flags;
-#endif
 	struct list_head	s_mounts;	/* list of mounts; _not_ for fs use */
 	struct block_device	*s_bdev;
 	struct backing_dev_info *s_bdi;
