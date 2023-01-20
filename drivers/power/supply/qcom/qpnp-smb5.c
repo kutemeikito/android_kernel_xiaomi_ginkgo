6f3b4b214022d376bb82508b12fc3f97e5a6fbea
siimsek <malisimsek17@gmail.com>
2023-01-20 05:06:40 -0500

    power: qpnp-smb5: silence thermal log
    [21181.679808] thermal_notifier_callback 3703
    
    Signed-off-by: siimsek <malisimsek17@gmail.com>
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -3731,7 +3731,6 @@ static int thermal_notifier_callback(struct notifier_block *noti, unsigned long 
 	struct fb_event *ev_data = data;
 	struct smb_charger *chg = container_of(noti, struct smb_charger, notifier);
 	int *blank;
-
 	if (ev_data && ev_data->data && chg) {
 		blank = ev_data->data;
 		if (event == MSM_DRM_EARLY_EVENT_BLANK && *blank == MSM_DRM_BLANK_UNBLANK) {
