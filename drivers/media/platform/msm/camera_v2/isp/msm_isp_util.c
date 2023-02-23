476767e3b191da01ca0e3656b0479ef61ff223a2
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-02-23 22:01:07 +0700

    drivers: camera: Add switch for enable/disable boot clock timestamp
    Our kernel currently supports both AOSP and MIUI ROMs, however
    the current AOSP ROM does not include the stock libcameraservice
    blob and it uses the timestamp buffer of the system monotonic
    time. If using the timestamp buffer of the boot clock this
    results in duration issue when recording video, so let's add
    switch to enable/disable the timestamp buffer of boot time.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -215,7 +215,11 @@ void msm_isp_get_timestamp(struct msm_isp_timestamp *time_stamp,
 		time_stamp->buf_time.tv_sec    = time_stamp->vt_time.tv_sec;
 		time_stamp->buf_time.tv_usec   = time_stamp->vt_time.tv_usec;
 	} else {
+#ifdef CONFIG_MSM_CAMERA_BOOTCLOCK_TIMESTAMP
+		ktime_get_ts(&ts);
+#else
 		get_monotonic_boottime(&ts);
+#endif
 		time_stamp->buf_time.tv_sec    = ts.tv_sec;
 		time_stamp->buf_time.tv_usec   = ts.tv_nsec/1000;
 		time_stamp->buf_time_ns        =
