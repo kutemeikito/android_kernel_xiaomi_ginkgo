ff96d03badf190e97a43f49ceae015f4ac84e806
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2023-08-03 17:02:36 +0700

    build: Update build compiler
    Big Thanks for @siimsek for reference
    
    Co-authored-by: Muhammed Ali Simsek <malisimsek17@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2020 Edwiin Kusuma Jaya (ryuzenn)
+# Copyright (C) 2023 Edwiin Kusuma Jaya (ryuzenn)
 #
 # Simple Local Kernel Build Script
 #
@@ -10,78 +10,79 @@
 #
 # Use this script on root of kernel directory
 
-sudo ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
-bold=$(tput bold)
-normal=$(tput sgr0)
+SECONDS=0 # builtin bash timer
+ZIPNAME="RyzenKernel-AOSP-Ginkgo-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
+TC_DIR="/workspace/toolchain/r487747"
+GCC_64_DIR="/workspace/toolchain/aarch64-linux-android-4.9"
+GCC_32_DIR="/workspace/toolchain/arm-linux-androideabi-4.9"
+AK3_DIR="/workspace/android/AnyKernel3"
+DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
-# Scrip option
-while (( ${#} )); do
-    case ${1} in
-        "-Z"|"--zip") ZIP=true ;;
-    esac
-    shift
-done
-
-[[ -z ${ZIP} ]] && { echo "${bold}Gunakan -Z atau --zip Untuk Membuat Zip Kernel Installer${normal}"; }
-
-# ENV
-CONFIG=vendor/ginkgo-perf_defconfig
-KERNEL_DIR=$(pwd)
-PARENT_DIR="$(dirname "$KERNEL_DIR")"
-KERN_IMG="/home/ryuzenn/out-memer/arch/arm64/boot/Image.gz-dtb"
-DTBO_IMG="/home/ryuzenn/out-memer/arch/arm64/boot/dtbo.img"
+export PATH="$TC_DIR/bin:$PATH"
 export KBUILD_BUILD_USER="EdwiinKJ"
 export KBUILD_BUILD_HOST="RastaMod69"
-export PATH="/home/ryuzenn/clown/prebuilts/clang/host/linux-x86/clang-rastamod/bin:$PATH"
-export LD_LIBRARY_PATH="/home/ryuzenn/clown/prebuilts/clang/host/linux-x86/clang-rastamod/lib:$LD_LIBRARY_PATH"
-export KBUILD_COMPILER_STRING="$(/home/ryuzenn/clown/prebuilts/clang/host/linux-x86/clang-rastamod/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
-export out=/home/ryuzenn/out-memer
+export KBUILD_BUILD_VERSION="1"
 
-# Functions
-clang_build () {
-    make -j4 O=$out \
-                          ARCH=arm64 \
-                          CC="clang" \
-                          AR="llvm-ar" \
-                          NM="llvm-nm" \
-					      LD="ld.lld" \
-			              AS="llvm-as" \
-						  STRIP="llvm-strip" \
-			              OBJCOPY="llvm-objcopy" \
-			              OBJDUMP="llvm-objdump" \
-						  CLANG_TRIPLE=aarch64-linux-gnu- \
-                          CROSS_COMPILE=aarch64-linux-gnu-  \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-}
-
-# Build kernel
-make O=$out ARCH=arm64 $CONFIG > /dev/null
-echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
-clang_build
-
-if ! [ -a $KERN_IMG ]; then
-    echo "${bold}Build error, Tolong Perbaiki Masalah Ini${normal}"
-    exit 1
+if ! [ -d "${TC_DIR}" ]; then
+echo "Clang not found! Cloning to ${TC_DIR}..."
+if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r487747.git ${TC_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
 fi
+fi
+
+if ! [ -d "${GCC_64_DIR}" ]; then
+echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if ! [ -d "${GCC_32_DIR}" ]; then
+echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+cp out/defconfig arch/arm64/configs/$DEFCONFIG
+exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -d "$AK3_DIR" ]; then
+cp -r $AK3_DIR AnyKernel3
+elif ! git clone -q https://github.com/kutemeikito/AnyKernel3; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
+fi
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
+rm -f *zip
+cd AnyKernel3
+git checkout master &> /dev/null
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+cd ..
+rm -rf AnyKernel3
+rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-
-[[ -z ${ZIP} ]] && { exit; }
-
-# clone AnyKernel3
-if ! [ -d "AnyKernel3" ]; then
-    git clone https://github.com/kutemeikito/AnyKernel3 -b AOSP
+echo "Zip: $ZIPNAME"
 else
-    echo "${bold}Direktori AnyKernel3 Sudah Ada, Tidak Perlu di Clone${normal}"
+echo -e "\nCompilation failed!"
+exit 1
 fi
-
-# ENV
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
-VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
-STRIP="aarch64-linux-gnu-strip"
-
-# Make zip
-make -C "$ZIP_DIR" clean
-wifi_modules
-cp "$KERN_IMG" "$DTBO_IMG" "$ZIP_DIR"/
-make -C "$ZIP_DIR" normal
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
