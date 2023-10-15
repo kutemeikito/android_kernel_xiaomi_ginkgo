85c2a2f86f295ab6111cb86b2df72fd9f81569c8
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2023-10-15 15:55:45 +0700

    Revert "mm: protect free_pgtables with mmap_lock write lock in exit_mmap"
    CLO has already picked this commit, so just revert it.
    
    This reverts commit fa10a5bf90edc5228022dacac3b6e8a6be836add.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3198,9 +3198,10 @@ void exit_mmap(struct mm_struct *mm)
 		__oom_reap_task_mm(mm);
 
 		set_bit(MMF_OOM_SKIP, &mm->flags);
+		down_write(&mm->mmap_sem);
+		up_write(&mm->mmap_sem);
 	}
 
-	down_write(&mm->mmap_sem);
 	if (mm->locked_vm) {
 		vma = mm->mmap;
 		while (vma) {
@@ -3213,11 +3214,8 @@ void exit_mmap(struct mm_struct *mm)
 	arch_exit_mmap(mm);
 
 	vma = mm->mmap;
-	if (!vma) {
-		/* Can happen if dup_mmap() received an OOM */
-		up_write(&mm->mmap_sem);
+	if (!vma)	/* Can happen if dup_mmap() received an OOM */
 		return;
-	}
 
 	lru_add_drain();
 	flush_cache_mm(mm);
@@ -3228,14 +3226,16 @@ void exit_mmap(struct mm_struct *mm)
 	free_pgtables(&tlb, vma, FIRST_USER_ADDRESS, USER_PGTABLES_CEILING);
 	tlb_finish_mmu(&tlb, 0, -1);
 
-	/* Walk the list again, actually closing and freeing it. */
+	/*
+	 * Walk the list again, actually closing and freeing it,
+	 * with preemption enabled, without holding any MM locks.
+	 */
 	while (vma) {
 		if (vma->vm_flags & VM_ACCOUNT)
 			nr_accounted += vma_pages(vma);
 		vma = remove_vma(vma);
 		cond_resched();
 	}
-	up_write(&mm->mmap_sem);
 	vm_unacct_memory(nr_accounted);
 }
 
