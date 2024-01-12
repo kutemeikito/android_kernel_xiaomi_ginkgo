d4804b64ba80cc28752a1e4e2642178e415b418d
Alexander Winkowski <dereference23@outlook.com>
2024-01-12 04:50:54 +0000

    Revert "ANDROID: mm/filemap: Fix missing put_page() for speculative page fault"
    This reverts commit b4e4425181dc524c8f823001eea57e1a9c93d2aa.
    
    Change-Id: I6d083e1e4a70a352cdf8162e72c1f3bfb1cc0b64
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2503,14 +2503,11 @@ int filemap_fault(struct vm_fault *vmf)
 
 	if (vmf->flags & FAULT_FLAG_SPECULATIVE) {
 		page = find_get_page(mapping, offset);
-		if (unlikely(!page))
+		if (unlikely(!page) || unlikely(PageReadahead(page)))
 			return VM_FAULT_RETRY;
 
-		if (unlikely(PageReadahead(page)))
-			goto page_put;
-
 		if (!trylock_page(page))
-			goto page_put;
+			return VM_FAULT_RETRY;
 
 		if (unlikely(compound_head(page)->mapping != mapping))
 			goto page_unlock;
@@ -2542,8 +2539,6 @@ int filemap_fault(struct vm_fault *vmf)
 		return VM_FAULT_LOCKED;
 page_unlock:
 		unlock_page(page);
-page_put:
-		put_page(page);
 		return VM_FAULT_RETRY;
 	}
 
