6378641a00eb816310677dd6e06cf3db74932c81
The_second_Tom <31297720+onlytominsecond@users.noreply.github.com>
2024-02-06 10:29:24 +0800

    BACKPORT: fs/namespace.c path_umount
    https://elixir.bootlin.com/linux/v5.9/source/fs/namespace.c#L1728
    
    full KernelSU gki features on non-gki hurray!
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1743,6 +1743,8 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
+	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
