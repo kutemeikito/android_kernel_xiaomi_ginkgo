f7cbd803fa093c76f0407179763da7ed17dcd56a
Tim Zimmermann <tim@linux4.de>
2024-06-03 17:58:15 +0200

    Revert "net: fix unused variable warning in do_tcp_setsockopt()"
    This reverts commit 7abf8b09720539a0c4f2561eff4002278e8b27c0.
    
    Reason for revert: Android common kernel backported the related changes from 4.19
    
    Change-Id: I7fd74a9c39c896d2fac31cde0aef8a7fd3dd040f
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2523,6 +2523,7 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct inet_connection_sock *icsk = inet_csk(sk);
+	struct net *net = sock_net(sk);
 	int val;
 	int err = 0;
 
