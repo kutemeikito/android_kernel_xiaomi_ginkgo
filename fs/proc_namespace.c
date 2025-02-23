92d6ba848084dd293a2ef719c371bef2693cca00
simonpunk <simonpunk2016@gmail.com>
2025-02-23 16:06:17 +0800

    kernel: Remove support for Shamiko
    - Making mountinfo visible to ksu process somehow would leak traces on some devices
    - We should not make things compatible with any closed source softwares as their code can be updated at anytime and we do not know what to adapt to the changes
    - latest Shamiko may not rely on mountinfo anymore so user should still be able to use Shamiko with susfs
    - Making mountinfo invisible to all process makes susfs less debuggable again
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -21,10 +21,6 @@
 #include "pnode.h"
 #include "internal.h"
 
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_is_current_ksu_domain(void);
-#endif
-
 static unsigned mounts_poll(struct file *file, poll_table *wait)
 {
 	struct seq_file *m = file->private_data;
@@ -110,7 +106,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
 #endif
 
@@ -151,7 +147,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
 #endif
 
@@ -220,7 +216,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
 #endif
 
