6e5dd6c9200839506fcd9dec6f45885ee41ca42e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    syscall: Only spoof uname for root
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1220,9 +1220,11 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		!strncmp(current->comm, "netbpfload", 10) ||
 		!strncmp(current->comm, "uprobestatsbpfload", 18) ||
 	    !strncmp(current->comm, "netd", 4))) {
-		strcpy(tmp.release, "5.4.186");
-		pr_info("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+		if (current_uid().val == 0) {
+			strcpy(tmp.release, "5.4.186");
+			pr_debug("fake uname: %s/%d release=%s\n",
+				 current->comm, current->pid, tmp.release);
+		}
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
