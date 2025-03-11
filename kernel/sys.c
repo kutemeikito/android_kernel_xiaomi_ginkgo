867a3e3521b0b169d022ad98a12d585d2256550c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    sys_newuname: only spoof for root
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1215,12 +1215,13 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
-	if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
+	if (current_uid().val == 0 && 
+		(!strncmp(current->comm, "bpfloader", 9) ||
+		!strncmp(current->comm, "netbpfload", 10) ||
 		!strncmp(current->comm, "uprobestatsbpfload", 18) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	    !strncmp(current->comm, "netd", 4))) {
 		strcpy(tmp.release, "5.4.186");
-		pr_debug("fake uname: %s/%d release=%s\n",
+		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
