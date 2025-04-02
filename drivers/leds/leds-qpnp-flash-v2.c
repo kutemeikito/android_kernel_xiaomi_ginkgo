cd9100de526332dfd3051b04444d96d4a6cba02a
Flopster101 <nahuelgomez329@gmail.com>
2025-04-02 18:02:46 -0300

    leds: qpnp-flash-v2: Implement custom brightness control hack
    * Intended to be compatible with https://github.com/ZorEl212/android_kernel_xiaomi_ginkgo/commit/caa6a682ee3822276ee291baa6757975fb17c22f, but does not require opening a file from within the kernel (which was unsafe).
    * It's independent from the standard `brightness` attribute that's present on all LED class drivers.
    * Only for torch-class devices.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/leds/leds-qpnp-flash-v2.c
+++ drivers/leds/leds-qpnp-flash-v2.c
@@ -190,6 +190,7 @@
 #define	MAX_IRES_LEVELS					4
 #define	FLASH_BST_PWM_OVRHD_MIN_UV			300000
 #define	FLASH_BST_PWM_OVRHD_MAX_UV			600000
+#define DEFAULT_TORCH_STRENGTH 50
 
 /* notifier call chain for flash-led irqs */
 static ATOMIC_NOTIFIER_HEAD(irq_notifier_list);
@@ -245,6 +246,7 @@ struct flash_node_data {
 	u8				current_reg_val;
 	u8				strobe_ctrl;
 	u8				strobe_sel;
+	int				custom_brightness;
 	enum flash_led_type		type;
 	bool				led_on;
 };
@@ -1253,6 +1255,14 @@ static void qpnp_flash_led_node_set(struct flash_node_data *fnode, int value)
 	fnode->ires_idx = fnode->default_ires_idx;
 	fnode->ires_ua = fnode->default_ires_ua;
 
+	if (fnode->type == FLASH_LED_TYPE_TORCH) {
+		if (value > 0) {
+			prgm_current_ma = fnode->custom_brightness;
+		} else {
+			prgm_current_ma = 0;
+		}
+	}
+
 	prgm_current_ma = min(prgm_current_ma, fnode->max_current);
 	if (prgm_current_ma > max_ires_curr_ma_table[fnode->ires_idx]) {
 		/* find the matching ires */
@@ -1838,6 +1848,52 @@ static ssize_t qpnp_flash_led_max_current_show(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%d\n", max_current);
 }
 
+/* Custom brightness hack */
+static ssize_t custom_brightness_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct flash_node_data *fnode = container_of(led_cdev, struct flash_node_data, cdev);
+	struct qpnp_flash_led *led = dev_get_drvdata(&fnode->pdev->dev);
+	int value;
+
+	spin_lock(&led->lock);
+	value = fnode->custom_brightness;
+	spin_unlock(&led->lock);
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", value);
+}
+
+static ssize_t custom_brightness_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct led_classdev *led_cdev = dev_get_drvdata(dev);
+	struct flash_node_data *fnode = container_of(led_cdev, struct flash_node_data, cdev);
+	struct qpnp_flash_led *led = dev_get_drvdata(&fnode->pdev->dev);
+	int value, rc;
+
+	rc = kstrtoint(buf, 10, &value);
+	if (rc)
+		return rc;
+
+	spin_lock(&led->lock);
+	fnode->custom_brightness = value;
+	spin_unlock(&led->lock);
+
+	return size;
+}
+
+static DEVICE_ATTR_RW(custom_brightness);
+
+static struct attribute *torch_attrs[] = {
+	&dev_attr_custom_brightness.attr,
+	NULL,
+};
+
+static const struct attribute_group torch_attr_group = {
+	.attrs = torch_attrs,
+};
+
 /* sysfs attributes exported by flash_led */
 static struct device_attribute qpnp_flash_led_attrs[] = {
 	__ATTR(max_current, 0664, qpnp_flash_led_max_current_show, NULL),
@@ -1953,6 +2009,7 @@ static int qpnp_flash_led_parse_each_led_dt(struct qpnp_flash_led *led,
 			fnode->type = FLASH_LED_TYPE_FLASH;
 		} else if (!strcmp(temp_string, "torch")) {
 			fnode->type = FLASH_LED_TYPE_TORCH;
+			fnode->custom_brightness = DEFAULT_TORCH_STRENGTH;
 		} else {
 			pr_err("Wrong flash LED type\n");
 			return rc;
@@ -1961,6 +2018,9 @@ static int qpnp_flash_led_parse_each_led_dt(struct qpnp_flash_led *led,
 		pr_err("Unable to read flash LED label\n");
 		return rc;
 	}
+
+	if (fnode->type == FLASH_LED_TYPE_TORCH)
+		fnode->cdev.groups = (const struct attribute_group *[]) { &torch_attr_group, NULL };
 
 	rc = of_property_read_u32(node, "qcom,id", &val);
 	if (!rc) {
