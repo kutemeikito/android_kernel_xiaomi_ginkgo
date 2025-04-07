c1c6878d389a93a016e0277c0771d4893f7d64e0
Skyblueborb <tomaszborbely0710@gmail.com>
2025-04-07 17:48:48 +0200

    input: fingerprint: fpc1020: Silence debug print
    Unnecessary.
    
    Change-Id: I8dea4322eb316178cfe9d18e8489cafad7fff956
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/input/fingerprint/fpc1020/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1020/fpc1020_tee.c
@@ -517,8 +517,6 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
 
-	dev_dbg(fpc1020->dev, "%s\n", __func__);
-
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
 		//wake_lock_timeout(&fpc1020->ttw_wl,
 		//			msecs_to_jiffies(FPC_TTW_HOLD_TIME));
