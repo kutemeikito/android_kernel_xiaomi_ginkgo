4a5e94b15dbf97332626a242e136ef8ebdf5557b
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-16 11:30:27 +0300

    syscall: Sync uname with android12-5.4
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1221,7 +1221,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		!strncmp(current->comm, "uprobestatsbpfload", 18) ||
 	    !strncmp(current->comm, "netd", 4))) {
 		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.4.186");
+			strcpy(tmp.release, "5.4.290");
 			pr_debug("fake uname: %s/%d release=%s\n",
 				 current->comm, current->pid, tmp.release);
 		}
