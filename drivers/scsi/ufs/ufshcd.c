2fedefbb00d2487c383e691fc95eea5adf5cf0cb
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-07-23 10:36:59 +0000

    fixup! scsi: ufs: Correct the LUN used in eh_device_reset_handler() callback
    from commits:
    https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo/commit/30f2a89f9481f851bc68e51a1e7114392b052231#diff-b6370444c32afa2e55d9b6150f355ba6f4d20c5ed5da5399ea8295d323de8267R5547
    
    idk why it went back to before, and only changed that part, maybe im forgot to change it, so we returned it according to the commit
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7666,8 +7666,8 @@ static int ufshcd_eh_device_reset_handler(struct scsi_cmnd *cmd)
 	hba = shost_priv(host);
 
 	ufshcd_print_cmd_log(hba);
-	lrbp = &hba->lrb[tag];
-	err = ufshcd_issue_tm_cmd(hba, lrbp->lun, 0, UFS_LOGICAL_RESET, &resp);
+	lun = ufshcd_scsi_to_upiu_lun(cmd->device->lun);
+	err = ufshcd_issue_tm_cmd(hba, lun, 0, UFS_LOGICAL_RESET, &resp);
 	if (err || resp != UPIU_TASK_MANAGEMENT_FUNC_COMPL) {
 		if (!err)
 			err = resp;
