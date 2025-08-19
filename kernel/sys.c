5ed765e3689a0259a3b0db8c2226cdb3f174dfa9
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-08-19 14:21:15 +0000

    [SQUASH] Revert "syscall: Only spoof uname for root"
    This reverts commit 6e5dd6c9200839506fcd9dec6f45885ee41ca42e.
    
    Revert "sys_newuname: only spoof for root"
    
    This reverts commit 867a3e3521b0b169d022ad98a12d585d2256550c.
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1211,16 +1211,13 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (current_uid().val == 0 && 
-		(!strncmp(current->comm, "bpfloader", 9) ||
+	if (!strncmp(current->comm, "bpfloader", 9) ||
 		!strncmp(current->comm, "netbpfload", 10) ||
 		!strncmp(current->comm, "uprobestatsbpfload", 18) ||
-	    !strncmp(current->comm, "netd", 4))) {
-		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.4.290");
-			pr_debug("fake uname: %s/%d release=%s\n",
-				 current->comm, current->pid, tmp.release);
-		}
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "5.4.290");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			current->comm, current->pid, tmp.release);
 	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
