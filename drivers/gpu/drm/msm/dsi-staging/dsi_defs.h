tzuyu-xd/android_kernel_xiaomi_ginkgo-2

drm: msm: dsi-staging: Add support for High Brightness Mode
Author: Arian <arian.kulmer@web.de>
Date:   Wed Jun 24 15:44:22 2020 +0200

    drm: msm: Use type_map array index 0 for invalid hbm values

    Change-Id: I8b29ad7227be9f01f945af136747a5924b0b628a

[ghostrider-reborn] Adapted to ginkgo LCD HBM
Change-Id: Id133e30c7bf645386053c42d4d121a053b03bac7
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
[fiqri19102002: Added #ifdef for spesific device features]
Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

drivers/gpu/drm/msm/dsi-staging/dsi_defs.h
@@ -308,6 +308,12 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_POST_TIMING_SWITCH,
 	DSI_CMD_SET_QSYNC_ON,
 	DSI_CMD_SET_QSYNC_OFF,
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	DSI_CMD_SET_HBM1_ON,
+	DSI_CMD_SET_HBM2_ON,
+	DSI_CMD_SET_HBM3_ON,
+	DSI_CMD_SET_HBM_OFF,
+#endif
 	DSI_CMD_SET_MAX
 };
 

drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -218,6 +218,10 @@ struct dsi_panel {
 	bool sync_broadcast_en;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
+
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+	int hbm_mode;
+#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -337,5 +341,9 @@ struct dsi_panel *dsi_panel_ext_bridge_get(struct device *parent,
 int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
+
+#ifdef CONFIG_MACH_XIAOMI_GINKGO
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+#endif
 
 #endif /* _DSI_PANEL_H_ */

kutemeikito/android_kernel_xiaomi_ginkgo

Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

drivers/gpu/drm/msm/dsi-staging/dsi_defs.h
@@ -308,6 +308,12 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_POST_TIMING_SWITCH,
 	DSI_CMD_SET_QSYNC_ON,
 	DSI_CMD_SET_QSYNC_OFF,
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	DSI_CMD_SET_HBM1_ON,
+	DSI_CMD_SET_HBM2_ON,
+	DSI_CMD_SET_HBM3_ON,
+	DSI_CMD_SET_HBM_OFF,
+#endif
 	DSI_CMD_SET_MAX
 };
 

drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -218,6 +218,10 @@ struct dsi_panel {
 	bool sync_broadcast_en;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
+
+#ifdef CONFIG_MACH_XIAOMI_C3J
+	int hbm_mode;
+#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -337,5 +341,9 @@ struct dsi_panel *dsi_panel_ext_bridge_get(struct device *parent,
 int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
+
+#ifdef CONFIG_MACH_XIAOMI_C3J
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+#endif
 
 #endif /* _DSI_PANEL_H_ */